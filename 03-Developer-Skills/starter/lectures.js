753d2cc1c6ffe6022a64ed2b606607a6aaecf7a9
Alex Standfield <AlexStandfield@gmail.com>
2021-02-08 12:53:02 -0700

    Instlling node.js and setting up a dev environment

---
+++ 03-Developer-Skills/starter/lectures.js
@@ -0,0 +1,17 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// SETTING UP PRETTIER AND VS CODE
+const x = 23;
+// if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log();
+// Changes double quotes to single qoutes, changes arrow fucntions to now automatically put parentheses for the parameter.
+// Changed cl as a snippet(shortcut) for console.log()
+
+// INSTALLING NODE.JS AND SETTING UP A DEV ENVIRONMENT
+console.log(calcAge(1998));
+
+// LEARING HOW TO CODE
