9d85b48d9b92a737217f73c862172eeaf8aa0345
Alex Standfield <AlexStandfield@gmail.com>
2021-02-18 11:57:33 -0700

    Manipulating The CSS Styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,93 @@
 'use strict';
+
+/*
+PROJECT #1: GUESS MY NUMBER!
+
+console.log(document.querySelector('.message').textContent); // Reading the content of element message
+*/
+
+/*
+SELECTING AND MANIPULATING ELEMENTS
+
+document.querySelector('.message').textContent = '🎉 Correct Number!'; // Selecting and changing the content of element message
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); // the element guess is an input which is why we use .value in this situation
+*/
+
+/*
+HANDLING CLICK EVENTS
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  }
+});
+*/
+
+/*
+IMPLEMENTING THE GAME LOGIC
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+/*
+MANIPULATING THE CSS STYLES
+
+// These are the additions made to the above code ^^^
+document.querySelector('body').style.backgroundColor = '#60b347';
+
+document.querySelector('.number').style.width = '30rem';
+*/
+
+/*
+CODING CHALLENGE #1
+*/
