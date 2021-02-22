d47de7fa5e3c960646bff1b4e6378a0d273c45de
Alex Standfield <AlexStandfield@gmail.com>
2021-02-22 13:22:34 -0700

    Section 7 Complete

---
+++ 07-Pig-Game/starter/mySolution.js
@@ -0,0 +1,22 @@
+// My Solution for the New Game Functionality
+// btnNew.addEventListener('click', function () {
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.remove('player--winner');
+
+//   document.getElementById(`current--${activePlayer}`).textContent = 0;
+//   currentScore = 0;
+//   activePlayer = activePlayer === 0 ? 1 : 0;
+//   player0El.classList.add('player--active');
+//   player1El.classList.remove('player--active');
+
+//   score0El.textContent = 0;
+//   score1El.textContent = 0;
+//   diceEl.classList.add('hidden');
+
+//   scores[0] = 0;
+//   scores[1] = 0;
+//   currentScore = 0;
+//   activePlayer = 0;
+//   playing = true;
+// });
